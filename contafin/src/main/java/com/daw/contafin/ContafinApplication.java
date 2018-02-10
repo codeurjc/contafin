1389668f9f8193c3334ed9a86ec3da3a207e082a
marceladb <marcela_795@hotmail.es>
2018-02-10 14:19:19 +0100

    Project created and files organized

---
+++ contafin/src/main/java/com/daw/contafin/ContafinApplication.java
@@ -0,0 +1,12 @@
+package com.daw.contafin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContafinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContafinApplication.class, args);
+	}
+}
---
+++ contafin/src/test/java/com/daw/contafin/ContafinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daw.contafin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContafinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
