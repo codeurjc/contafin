98d5b1b42506df4be3afcbde5788c1f7da30e9e6
marceladb <marcela_795@hotmail.es>
2018-02-13 12:29:43 +0100

    Now lessons.html is implemented with Mustache

--- contafin/src/main/java/com/daw/contafin/lesson/LessonController.java
+++ contafin/src/main/java/com/daw/contafin/lesson/LessonController.java
@@ -1,5 +1,40 @@
 package com.daw.contafin.lesson;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LessonController {
+	
+	@Autowired
+	private LessonRepository repository;
+	
+	@RequestMapping("lessons")
+    public String lessons(Model model) {
+		
+		List<String> lesson = Arrays.asList("Iniciación","Patrimonio","Inmovilizado");
+		
+		model.addAttribute("name", "");
+		model.addAttribute("points", "");
+		model.addAttribute("streak", "");
+		model.addAttribute("done", "0");
+		model.addAttribute("total", "3");
+		model.addAttribute("lesson", lesson);
+		model.addAttribute("title", "Introducción");
+		
+    	return "lessons";
+    }
+
+	@RequestMapping("lessons/{{-index}}")
+    public String exercisePage() {
+		
+    	return "exerciseTemplate";
+    }
+	
 
 }
