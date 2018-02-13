ca792cb23a7427a80b8ec36de29658f5bb926e0e
marceladb <marcela_795@hotmail.es>
2018-02-13 11:48:04 +0100

    Added relationship with CompletedExercise

--- contafin/src/main/java/com/daw/contafin/student/Student.java
+++ contafin/src/main/java/com/daw/contafin/student/Student.java
@@ -1,9 +1,14 @@
 package com.daw.contafin.student;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.daw.contafin.exercise.Exercise;
 
 @Entity
 public class Student {
@@ -20,6 +25,10 @@ public class Student {
 	private int points;
 	private int streak;
 	private int fluency;
+	
+	@OneToMany (mappedBy="student")
+	private List<Exercise> exercises;
+	
 	
 	public Student() {
 		
