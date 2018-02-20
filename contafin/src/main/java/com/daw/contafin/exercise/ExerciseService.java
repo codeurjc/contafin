5af153d422072875773a0cd54b3a6fc12c00596a
DESKTOP-4JKJA9V\Luisc <luiscaumel@gmail.com>
2018-02-20 09:58:38 +0100

    Now Controller is conect with ExerciseService

---
+++ contafin/src/main/java/com/daw/contafin/exercise/ExerciseService.java
@@ -0,0 +1,37 @@
+package com.daw.contafin.exercise;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daw.contafin.lesson.Lesson;
+
+@Service
+public class ExerciseService {
+
+	
+		@Autowired
+		ExerciseRepository exerciseRepository;
+		
+		
+		public Exercise findById (long id) {
+			return exerciseRepository.findById(id);
+		}
+		
+		public Exercise findByLessonAndId (Lesson lesson, long id) {
+			return exerciseRepository.findByLessonAndId(lesson,id);
+		}
+		
+		public Exercise findByLessonAndKind(Lesson lesson, int kind) {
+			return exerciseRepository.findByLessonAndKind(lesson,kind);
+		}
+		
+		public List <Exercise> findByLesson(Lesson lesson) {
+			 return exerciseRepository.findByLesson(lesson);
+		}
+		
+		
+		
+		
+}
