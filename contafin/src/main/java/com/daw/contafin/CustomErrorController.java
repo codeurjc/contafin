70c771539429113b4df46b7fc03dbe704961333c
Jaime Morillo <jaimorile@hotmail.es>
2018-02-22 21:06:58 +0100

    Add custom Error Controller
    Is commented now. You can use to create dynamic error.html with
    mustache.

---
+++ contafin/src/main/java/com/daw/contafin/CustomErrorController.java
@@ -0,0 +1,31 @@
+package com.daw.contafin;
+
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+//public class CustomErrorController implements ErrorController {
+public class CustomErrorController {
+/*
+    private static final String PATH = "/error";
+
+
+    @RequestMapping(value = PATH)
+    String error(Model model) {
+    
+    model.addAttribute("error",error);
+    
+        return "error2";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+*/
+}
