eb171c71343f3ee776d0cd0c6d00dcc8fb2864b9
marceladb <marcela_795@hotmail.es>
2018-02-24 18:31:51 +0100

    Added export to Excel.
    -Used library: Apache POI.

--- contafin/src/main/java/com/daw/contafin/admin/AdminController.java
+++ contafin/src/main/java/com/daw/contafin/admin/AdminController.java
@@ -1,5 +1,41 @@
 package com.daw.contafin.admin;
 
-public class AdminController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.daw.contafin.ExcelExport;
+import com.daw.contafin.user.User;
+import com.daw.contafin.user.UserService;
+
+
+@Controller
+@RequestMapping ("Admin")
+public class AdminController {
+	
+	@Autowired
+	UserService userService;
+
+	
+	@RequestMapping ("ExportPreview")
+	public ModelAndView userData (ModelAndView model) {
+		
+		 List<User> users = userService.getUsers();
+		return new ModelAndView("exportPreview", "userList", users);
+	}
+	 
+	 @RequestMapping(value="Export", method=RequestMethod.GET)
+	 public ModelAndView userListReport(){
+	  
+		 List<User> users = userService.getUsers();
+		 return new ModelAndView(new ExcelExport(), "userList", users);
+	  
+	 }
+	 
+	
 }
+	 
