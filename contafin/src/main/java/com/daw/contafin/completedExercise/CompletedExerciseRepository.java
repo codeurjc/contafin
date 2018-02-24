217028f786e5356c2ba6733ede934544ee1651f7
Jaime Morillo <jaimorile@hotmail.es>
2018-02-24 22:46:29 +0100

    Update exercisesRepository and Service

--- contafin/src/main/java/com/daw/contafin/completedExercise/CompletedExerciseRepository.java
+++ contafin/src/main/java/com/daw/contafin/completedExercise/CompletedExerciseRepository.java
@@ -1,6 +1,8 @@
 package com.daw.contafin.completedExercise;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.daw.contafin.exercise.Exercise;
@@ -8,5 +10,6 @@ import com.daw.contafin.user.User;
 
 
 public interface CompletedExerciseRepository extends JpaRepository <CompletedExercise, Long>{
-	//CompletedExercise findByUserAndExercise(User user, Exercise exercise);
+	CompletedExercise findByUserAndExercise(User user, Exercise exercise);
+	List<CompletedExercise> findByUser(User user);
 }
--- contafin/src/main/java/com/daw/contafin/completedExercise/CompletedExerciseService.java
+++ contafin/src/main/java/com/daw/contafin/completedExercise/CompletedExerciseService.java
@@ -1,7 +1,11 @@
 package com.daw.contafin.completedExercise;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.daw.contafin.exercise.Exercise;
+import com.daw.contafin.user.User;
 
 @Service
 public class CompletedExerciseService {
@@ -11,6 +15,9 @@ public class CompletedExerciseService {
 	
 	public void save(CompletedExercise completedExercise) {
 		completedExerciseRepository.save(completedExercise);
+	}
+	public CompletedExercise findByUserAndExercise(User user, Exercise exercise){
+		return completedExerciseRepository.findByUserAndExercise(user,exercise);
 	}
 	
 }
