eab297974f5a550e5fc3ca51a2f1325807304479
raulvr2 <32631567+raulvr2@users.noreply.github.com>
2018-02-26 11:12:51 +0100

    Custom error controller

--- contafin/src/main/java/com/daw/contafin/CustomErrorController.java
+++ contafin/src/main/java/com/daw/contafin/CustomErrorController.java
@@ -1,36 +1,31 @@
 package com.daw.contafin;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-//public class CustomErrorController implements ErrorController {
-	public class CustomErrorController {
-/*
+public class CustomErrorController implements ErrorController {
+
 	@Autowired
 	ErrorMessage errorMessage;
-	
-    private static final String PATH = "/error";
 
+	private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
-    String error(Model model) {
-    
-    model.addAttribute("errorMessage",errorMessage.getMessage());
-    errorMessage.setMessage("The web page doesn't exist or you don't have permission.");
-    
-        return "error2";
-    }
+	@RequestMapping(value = PATH)
+	String error(Model model) {
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
-    }
-*/
+		model.addAttribute("errorMessage", errorMessage.getMessage());
+		errorMessage.setMessage("The web page doesn't exist or you don't have permission.");
+
+		return "error2";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return PATH;
+	}
 
 }
