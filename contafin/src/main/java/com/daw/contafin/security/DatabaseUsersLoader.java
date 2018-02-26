3fa8a853927d31701e77e05e80e131420eb9edbb
AlejandroPinto <ale.pinto.madrid@gmail.com>
2018-02-26 08:46:28 +0100

    Arranged code format

--- contafin/src/main/java/com/daw/contafin/security/DatabaseUsersLoader.java
+++ contafin/src/main/java/com/daw/contafin/security/DatabaseUsersLoader.java
@@ -8,24 +8,23 @@ import org.springframework.stereotype.Component;
 import com.daw.contafin.user.User;
 import com.daw.contafin.user.UserRepository;
 
-
 @Component
 public class DatabaseUsersLoader {
-	
-	@Autowired
-    private UserRepository userRepository;
 
-    @PostConstruct
-    private void initDatabase() {
-    		
-    		userRepository.save(new User("user","email@hotmail.es", "pass", "ROLE_USER"));
-		userRepository.save(new User("admin","adminemail@hotmail.es", "adminpass", "ROLE_ADMIN"));
-		userRepository.save(new User("Sergio", "sergio@hotmail.es", "pass",2 ,50 ,7 , 3 , "ROLE_USER"));
-		
-		//Add test users.
-		for (int i=0; i<=40; i++) {
-			userRepository.save(new User("user "+i,"email"+i+"@hotmail.es", "pass", "ROLE_USER"));
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	private void initDatabase() {
+
+		userRepository.save(new User("user", "email@hotmail.es", "pass", "ROLE_USER"));
+		userRepository.save(new User("admin", "adminemail@hotmail.es", "adminpass", "ROLE_ADMIN"));
+		userRepository.save(new User("Sergio", "sergio@hotmail.es", "pass", 2, 50, 7, 3, "ROLE_USER"));
+
+		// Add test users.
+		for (int i = 0; i <= 40; i++) {
+			userRepository.save(new User("user " + i, "email" + i + "@hotmail.es", "pass", "ROLE_USER"));
 		}
-    }
+	}
 
 }
