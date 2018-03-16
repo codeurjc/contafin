e4eb313a80595c55703d3f44340a842ab399e1ef
Jaime Morillo <jaimorile@hotmail.es>
2018-03-16 11:48:00 +0100

    Docker update, now you can compile the project and build the image properly

---
+++ docker/create_image.ps1
@@ -0,0 +1,14 @@
+$var = $(get-item ${PWD}).parent.FullName
+$pathProject = $var + "\contafin"
+$pathJar = $pathProject + "\target"
+
+Write-Output "Creating jar of contafin..."
+#Create jar contafin
+docker run -it --rm --name contafin -v ${pathProject}:/usr/src/mymaven -w /usr/src/mymaven maven mvn clean package -DskipTests
+
+#Move jar to actual directory
+mv ${pathJar}/contafin-0.0.1-SNAPSHOT.jar .
+
+Write-Output "Creating Docker image of contafin..."
+#Creating image 
+docker build -t contafin .
