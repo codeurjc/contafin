f929f3ac679f329629e6003f5040160098a5479d
DESKTOP-4JKJA9V\Luisc <luiscaumel@gmail.com>
2018-04-03 09:36:25 +0200

    Update Angular project

--- AngularContafin/e2e/app.e2e-spec.ts
+++ AngularContafin/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularContafinPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-contafin App', function() {
-  let page: AngularContafinPage;
+describe('angular-contafin App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularContafinPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
