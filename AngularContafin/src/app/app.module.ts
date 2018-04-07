cb4902024a1522167c81969a1744535df96badbd
Caumel <luiscaumel@gmail.com>
2018-04-07 12:52:59 +0200

    Add bootstrap and ng-bootstrap

--- AngularContafin/src/app/app.module.ts
+++ AngularContafin/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
