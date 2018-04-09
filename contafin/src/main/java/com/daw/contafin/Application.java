7cf125dfb49872191f767f750f0fed3d24d23cc6
Jaime Morillo <jaimorile@hotmail.es>
2018-04-09 12:02:24 +0200

    Add more configuration to CORS

--- contafin/src/main/java/com/daw/contafin/Application.java
+++ contafin/src/main/java/com/daw/contafin/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -40,5 +43,19 @@ public class Application {
 	            gen.writeEndObject();
 	        }
 	    });
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+						.allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin",
+								"Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+						.allowCredentials(true).maxAge(3600);
+			}
+		};
 	}
 }
