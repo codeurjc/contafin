ffe3662c0183f8d397bc21a9007bf1a0c759f75e
marceladb <marcela_795@hotmail.es>
2018-04-09 22:01:03 +0200

    Updated navbar.
    Almost finished.  To be done:
    - Add user profile picture.
    - Add admin button.

--- AngularContafin/src/app/navbar/navbar.component.ts
+++ AngularContafin/src/app/navbar/navbar.component.ts
@@ -3,20 +3,37 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { LoginService } from '../login/login.service';
+import { UserService } from '../user/user.service';
+import { User } from '../Interfaces/User/user.model';
+import { LoginComponent } from '../login/login.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavBarComponent  {
+export class NavBarComponent {
 
-  public isAdmin: boolean;
-  public isLoggedUser: boolean;
+  public isAdmin: boolean = false;
+  public isLoggedUser: boolean = false;
+  public loggedUser: User;
 
-  constructor(public loginService: LoginService){
-    this.isAdmin = this.loginService.isAdministrator();
+  constructor(private router: Router, public loginService: LoginService) {
+    this.loggedUser = this.loginService.getLoggedUser();
     this.isLoggedUser = this.loginService.isLoggedUser();
+    /*if (this.loginService.isLoggedUser()){
+      this.isAdmin = this.loggedUser.roles.indexOf('ROLE_ADMIN') !== -1;
+    }*/
   }
 
+  logOut() {
+    this.loginService.logOut().subscribe(
+      response => {
+        console.log('Logged out');
+        this.router.navigate(['/']);
+      },
+      error => console.log('Error when trying to log out: ' + error)
+    );
+  }
 
 }
