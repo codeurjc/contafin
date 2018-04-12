bcfbf89326f75f244aa514f04c0129a251644efa
Jaime Morillo <jaimorile@hotmail.es>
2018-04-12 10:46:32 +0200

    Update Unitservice

--- AngularContafin/src/app/unit/unit.service.ts
+++ AngularContafin/src/app/unit/unit.service.ts
@@ -19,26 +19,34 @@ export class UnitsService {
 	}
 
 
-	getUnit(id: number | string) {
+	getUnit(id: number) {
 		return this.http.get(BASE_URL + id)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
 
+	//Need the unit with its lessons and exercises
 	addUnit(unit: Unit) {
 		return this.http.post(BASE_URL, unit)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
 
-	removeUnit(unit: Unit) {
-		return this.http.delete(BASE_URL + unit.id)
+	//No va
+	/*uploadImages(id: number, images: Array<Images>) {
+		return this.http.post(BASE_URL+ id +"/Images", images)
+			.map(response => response.json())
+			.catch(error => this.handleError(error));
+	}*/
+
+	changeNameUnit(id: number, name:string) {
+		return this.http.put(BASE_URL + id, name)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
 
-	updateUnit(unit: Unit) {
-		return this.http.put(BASE_URL + unit.id, unit)
+	deleteUnit(id:number) {
+		return this.http.delete(BASE_URL + id)
 			.map(response => response.json())
 			.catch(error => this.handleError(error));
 	}
