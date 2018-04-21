5c543f24f74558db3d7fa977fd0a0283ba1fd75b
Caumel <luiscaumel@gmail.com>
2018-04-21 19:53:23 +0200

    now we get the exercises

--- AngularContafin/src/app/exercise/exercise.component.ts
+++ AngularContafin/src/app/exercise/exercise.component.ts
@@ -10,24 +10,28 @@ import { ExerciseService } from './exercise.service';
 })
 export class ExerciseComponent {
 
-  idunit: number;
-  idlesson: number;
-  idexercise: number;
+  idUnit: number;
+  idLesson: number;
+  idExercise: number;
+  idExercises: number[] = new Array();
   exercises: Exercise[] = new Array();
 
   constructor(private router: Router, activatedRoute: ActivatedRoute, public exerciseService: ExerciseService) {
-    let idunit = activatedRoute.snapshot.params['id'];
-    this.idunit = parseInt(idunit);
-    let idlesson = activatedRoute.snapshot.params['idlesson'];
-    this.idlesson = parseInt(idlesson);
-    let idexercise = activatedRoute.snapshot.params['idexercise'];
-    this.idexercise = parseInt(idexercise);
-    this.idlesson = (this.idunit-1)*3+this.idlesson;
-    this.idexercise = (this.idlesson-1)*3+this.idexercise;
-    this.getExercises(this.idunit,this.idlesson,this.idexercise);
-    this.getExercises(this.idunit,this.idlesson,this.idexercise+1);
-    this.getExercises(this.idunit,this.idlesson,this.idexercise+2);
-    this.getExercises(this.idunit,this.idlesson,this.idexercise+3);
+    let idUnit = activatedRoute.snapshot.params['id'];
+    this.idUnit = parseInt(idUnit);
+    let idLesson = activatedRoute.snapshot.params['idlesson'];
+    this.idLesson = parseInt(idLesson);
+    let idExercise = activatedRoute.snapshot.params['idexercise'];
+    this.idExercise = parseInt(idExercise);
+    this.idExercise = (((this.idUnit-1)*3+this.idLesson)-1)*4+this.idExercise;
+    this.idExercises.push(this.idExercise);
+    this.idExercises.push(this.idExercise+1);
+    this.idExercises.push(this.idExercise+2);
+    this.idExercises.push(this.idExercise+3);
+    this.getExercises(this.idUnit,this.idLesson,this.idExercise);
+    this.getExercises(this.idUnit,this.idLesson,this.idExercise+1);
+    this.getExercises(this.idUnit,this.idLesson,this.idExercise+2);
+    this.getExercises(this.idUnit,this.idLesson,this.idExercise+3);
   }
 
   getExercises(idunit: number, idlesson: number, idexercise: number) {
@@ -39,9 +43,10 @@ export class ExerciseComponent {
 
 
   pulsar() {
-    console.log(this.idunit);
-    console.log(this.idlesson);
-    console.log(this.idexercise);
+    console.log(this.idUnit);
+    console.log(this.idLesson);
+    console.log(this.idExercise);
+    console.log(this.idExercises);
     console.log(this.exercises);
   }
 
